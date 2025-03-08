6e61e6b4a8c4ab02b38e4e7ac9e4f94d8fb40ca2
AviAsnani <aviasnani@MACBOOKAIR-3E78.station>
2025-03-08 14:18:43 +0000

    added ssh pub key to secrets

--- terraform/key_pair.tf
+++ terraform/key_pair.tf
@@ -1,4 +1,10 @@
+variable "ssh_public_key" {
+  description = "SSH public key to create the key pair"
+  type        = string
+}
+
+
 resource "aws_key_pair" "hello_key" {
-  key_name   = "hellothisismykey"  # Name of the key pair
-  public_key = file("~/.ssh/id_rsa.pub")  # Path to your public key file (adjust if needed)
+  key_name   = "hellothisismykey"  
+  public_key = var.ssh_public_key  
 }
